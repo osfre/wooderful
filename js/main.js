a762d08f6d349340239f8c5a9355add96b3b5b36
osfre <freedosama34@gmail.com>
2023-03-09 13:42:32 +0200

    final edit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
+  // swiper1
+  var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 0,
     slidesPerGroup: 1,
@@ -31,69 +32,74 @@ var swiper = new Swiper(".mySwiper", {
         },
       },
   });
-var swiper = new Swiper(".mySwiper1", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    slidesPerGroup: 1,
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false
-    },
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
+  // swiper1
+  // swiper2
+  var swiper = new Swiper(".mySwiper1", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
       },
-  });
-
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+        },
+    });
+// swiper2
+// scroll
   let btn0 = document.querySelector(".goToNextSection1")
   let nav = document.querySelector(".navbar")
-btn0.onclick = () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-    })
-}
-onscroll = () => {
-  scrollToTop()
-  navbar()
-}
-function scrollToTop() {
-    if (window.scrollY >= 400) {
-        btn0.style.display = "flex";
-    }
-    else {
-        btn0.style.display = "none";
-    }
-}
-function navbar() {
-    if (window.scrollY >= 50) {
-        nav.classList.add("show")
+  btn0.onclick = () => {
+      window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth',
+      })
+  }
+  onscroll = () => {
+    scrollToTop()
+    navbar()
+  }
+  function scrollToTop() {
+      if (window.scrollY >= 400) {
+          btn0.style.display = "flex";
       }
       else {
-      nav.classList.remove("show")
-    }
-}
+          btn0.style.display = "none";
+      }
+  }
+  function navbar() {
+      if (window.scrollY >= 50) {
+          nav.classList.add("show")
+        }
+        else {
+        nav.classList.remove("show")
+      }
+  }
+// scroll
+// change name title
 let mainTitle = document.title;
 window.onblur = () => {
 	document.title = "Hey, Come Back";
@@ -101,10 +107,10 @@ window.onblur = () => {
 window.onfocus = () => {
 	document.title = mainTitle;
 }
-
+// change name title
+// name 
 let myName = "وودرفول";
 let index = 0;
-
 function writeText() {
   document.querySelector(".name").textContent = myName.slice(0,index);
   index++;
@@ -112,13 +118,11 @@ function writeText() {
     index = 1;
   }
 }
-
 setInterval(() => {
   writeText();
 }, 500);
-
-
-
+// name 
+// remove nav link
 let btn = document.querySelector(".navbar-collapse");
 let links = document.querySelectorAll(".navbar-nav .nav-link")
 
@@ -127,3 +131,14 @@ links.forEach(link => {
     btn.classList.remove("show");
   }
 });
+// remove nav link
+// Initialize library
+lozad('.lozad', {
+  load: function(el) {
+    el.src = el.dataset.src;
+      el.onload = function() {
+        el.classList.add('fade-test')
+      }
+  }
+}).observe()
+      // Initialize library
